2ec141da49cefe378f31191f8b21107b0b441e2d
Tasleem <tasleembca31@gmail.com>
2023-06-25 15:53:04 +0530

    testing spring boot app added

---
+++ src/main/java/com/javatechie/jpa/SpringDataJpaMappingApplication.java
@@ -0,0 +1,15 @@
+package com.javatechie.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMappingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringDataJpaMappingApplication.class, args);
+		System.out.println("=========== Application Started ===========");
+	}
+
+}
